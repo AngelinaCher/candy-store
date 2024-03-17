2057dcc42089a80498a77f23ca36d449c2921e8d
Angelina_RLY <cher.rly2036@gmail.com>
2024-03-17 16:17:08 +0300

    Доработка моделей корзины и заказа

--- candy_store_server/cart/models.py
+++ candy_store_server/cart/models.py
@@ -1,6 +1,5 @@
 import uuid
 from django.db import models
-from orders.models import Order
 from users.models import CustomUser
 from candy_store.models import Product
 
@@ -12,26 +11,33 @@ class PaymentMethod(models.TextChoices):
 
 class Cart(models.Model):
     cart_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    order_id = models.ForeignKey(to=Order, on_delete=models.PROTECT, null=False, blank=False,
-                                 verbose_name='Номер заказа')
-    user_id = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE, null=False, blank=False,
-                                verbose_name='Пользователь')
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE, null=False, blank=False,
                                    verbose_name='Товар')
     quantity = models.IntegerField(verbose_name='Количество')
     discount = models.PositiveIntegerField(verbose_name='Скидка', null=True, default=None)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, verbose_name='Общая сумма')
-    date_added = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
-    is_active = models.BooleanField(default=True, verbose_name='Активность корзины')
     notes = models.TextField(blank=True, null=True, verbose_name='Комментарий пользователя')
-    payment_method = models.CharField(max_length=255, blank=True, choices=PaymentMethod.choices,
-                                      verbose_name='Способ оплаты')
-    discount_code = models.CharField(max_length=255, blank=True, verbose_name='Промокод')
+    created_up = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
+    updated_up = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
 
     def __str__(self):
         return self.cart_id
 
-
     class Meta:
         verbose_name = "Корзина"
         verbose_name_plural = "Корзины"
+
+
+class CartItem(models.Model):
+    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart_items', verbose_name='Корзина')
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, null=False, blank=False,
+                                   verbose_name='Товар')
+    product_quantity = models.IntegerField(default=1, verbose_name='Количество товара')
+    user_id = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE, null=False, blank=False,
+                                verbose_name='Пользователь')
+
+    def __str__(self):
+        return f'Корзина: {self.cart_id.cart_id}, товар: {self.product_id.product_name}'
+
+    class Meta:
+        verbose_name = "Товар корзины"
+        verbose_name_plural = "Товары корзины"
--- candy_store_server/orders/models.py
+++ candy_store_server/orders/models.py
@@ -1,18 +1,32 @@
 import uuid
 from django.db import models
 from users.models import CustomUser
+from cart.models import Cart
 
 
 class Order(models.Model):
-    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    customer_id = models.ForeignKey(to=CustomUser, on_delete=models.PROTECT, null=False, blank=False,
+    order_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name='Заказ')
+    customer_id = models.ForeignKey(to=CustomUser, on_delete=models.CASCADE, null=False, blank=False,
                                     verbose_name='Пользователь')
+    cart_id = models.ForeignKey(to=Cart, on_delete=models.CASCADE, verbose_name='Козина')
     order_date = models.DateTimeField(verbose_name='Дата заказа', auto_now_add=True, null=False)
     required_date = models.DateTimeField(verbose_name='Требуемая дата')
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма')
     status = models.CharField(max_length=20, choices=(
         ('new', 'Новый'),
         ('in_progress', 'В обработке'),
         ('confirmed', 'Подтверждён'),
-        ('completed', 'Выполнен')), default='new')
+        ('completed', 'Выполнен')), default='new', verbose_name='Статус заказа')
+    payment_method = models.CharField(max_length=20, choices=(
+        ('CH', 'Наличные'),
+        ('BC', 'Банковская карта'),
+    ))
+    created_up = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
+    updated_up = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
 
+    def __str__(self):
+        return self.order_id
 
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = "Заказы"
