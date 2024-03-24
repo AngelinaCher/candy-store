cf8b41009b950c119ab6d53ad1047fbecc0338a9
abaisseabc <abaisseabc666@gmail.com>
2024-03-24 19:46:15 +0300

    init(candy_store_client): инициализация клиентской части

---
+++ candy_store_client/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainPage",
+}
+</script>
+
+<style lang="scss">
+@import "./assets/styles/main.scss";
+</style>
