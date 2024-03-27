9e385845463dbe9c221dbf065d7037e857a2ffff
abaisseabc <abaisseabc666@gmail.com>
2024-03-27 21:08:26 +0300

    feat(candy_store_client): добавлены категории на главную страницу

---
+++ candy_store_client/src/components/main-page/Categories.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main-page_categories">
+    <router-link
+        class="main-page_categories_category"
+        v-for="cat in testData"
+        :key="cat.id" to="/"
+    >
+      <h1>{{ cat["category"] }}</h1>
+      <p>{{ cat["desc"] }}</p>
+      <!--      <img :src="require(`${cat.img}`)" alt="">-->
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Categories",
+  data() {
+    return {
+      testData: [
+        {"id": 1, "category": "Батончики", "desc": "Питательные для активного образа жизни", "img": "",},
+        {"id": 2, "category": "Шоколад", "desc": "Нежные и ароматные, для сладкого утешения", "img": "",},
+        {"id": 3, "category": "Печенье", "desc": "Хрустящие и нежные для домашнего уюта", "img": "",},
+        {"id": 4, "category": "Мармелад", "desc": "Сладкие, сочные, для радости и удовольствия", "img": "",},
+        {"id": 5, "category": "Сладкие подарки", "desc": "Радостные комплекты, для особых моментов", "img": "",},
+        {"id": 6, "category": "Конфеты", "desc": "Роскошные, изысканные, для настоящих гурманов", "img": "",}
+      ]
+    }
+  }
+}
+</script>
+
--- candy_store_client/src/views/MainPage.vue
+++ candy_store_client/src/views/MainPage.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="main-page">
-    <Bannner />
+    <Bannner/>
+    <Categories />
   </div>
 </template>
 
 <script>
 import Bannner from "../components/main-page/Banner.vue";
+import Categories from "../components/main-page/Categories.vue";
 
 export default {
-    name: "MainPage",
-    components: {
-        Bannner,
-    }
+  name: "MainPage",
+  components: {
+    Bannner,
+    Categories,
+  }
 }
 </script>
