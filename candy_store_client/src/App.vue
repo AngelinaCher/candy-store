76ee6f99391ba8818b671217727b62f24a0140e7
abaisseabc <abaisseabc666@gmail.com>
2024-03-27 21:54:53 +0300

    feat(candy_store_client): добавлен footer

--- candy_store_client/src/App.vue
+++ candy_store_client/src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <AppHeader/>
     <router-view/>
+    <AppFooter />
   </div>
 </template>
 
 <script>
 import AppHeader from "./components/app/AppHeader.vue";
+import AppFooter from "./components/app/AppFooter.vue";
 
 export default {
   name: "MainPage",
   components: {
     AppHeader,
-  }
+    AppFooter,
+  },
 }
 </script>
 
---
+++ candy_store_client/src/components/app/AppFooter.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app-footer">
+    <div class="app-footer_main">
+      <h1>Карамелька</h1>
+    </div>
+    <hr>
+    <div class="app-footer_contacts">
+      <p style="margin-right: 700px">© 2019-2024. ИП Чернобровкина Лидия Александровна</p>
+      <p>karamelka@gmail.com</p>
+      <p>+7 (918) 343-33-56</p>
+      <p>г. Краснодар, ул. Рашпилевская, 137</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppFooter",
+}
+</script>
