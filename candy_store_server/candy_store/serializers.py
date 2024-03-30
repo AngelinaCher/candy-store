b568516a40585f8904d418a49812337a63eb514e
Angelina_RLY <cher.rly2036@gmail.com>
2024-03-30 12:43:32 +0300

    api Categories

---
+++ candy_store_server/candy_store/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from candy_store.models import Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['category_id', 'category_name', 'slug', ]
--- candy_store_server/candy_store/views.py
+++ candy_store_server/candy_store/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from candy_store.serializers import CategorySerializer
+from candy_store.models import Category
 
-# Create your views here.
+
+class CategoryListView(ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
