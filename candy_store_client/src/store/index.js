deec08500475789dd2c5c5d859ebb3c97da560c1
abaisseabc <abaisseabc666@gmail.com>
2024-05-18 12:08:14 +0300

    feat: добавлена возможность авторизации пользователя,  инициализация токенов при монтировании приложения

--- candy_store_client/src/store/index.js
+++ candy_store_client/src/store/index.js
@@ -1,12 +1,68 @@
 import Vue from "vue";
 import Vuex from "vuex";
 
+import axios from 'axios';
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-    state: {},
+    state: {
+        tokenData: {
+            "access_token": "",
+            "refresh_token": "",
+            "type_token": "Bearer",
+        },
+        isAuthenticated: false,
+    },
     getters: {},
-    mutations: {},
+    mutations: {
+        setToken(state, tokenData) {
+            state.tokenData["access_token"] = tokenData["access"];
+            state.tokenData["refresh_token"] = tokenData["refresh"];
+            state.isAuthenticated = true;
+        },
+        async initializeStore(state) {
+            if (localStorage.getItem('access_token') && localStorage.getItem('refresh_token')) {
+                await axios
+                    .post("http://127.0.0.1:8000/api/v1/auth/jwt/verify/", {token: localStorage.getItem("access_token")})
+                    .then(response => {
+                        state.tokenData['access_token'] = localStorage.getItem('access_token');
+                        state.tokenData['refresh_token'] = localStorage.getItem('refresh_token');
+                        axios.defaults.headers.common["Authorization"] = `Bearer ${state.tokenData['access_token']}`;
+                        state.isAuthenticated = true;
+                    })
+                    .catch(err => {
+                        if (err["message"] === "Request failed with status code 401") {
+                            axios.post(
+                                "http://127.0.0.1:8000/api/v1/auth/jwt/refresh/",
+                                {"refresh": localStorage.getItem('refresh_token')}
+                            )
+                                .then(response => {
+                                    if (response.data["access"]) {
+                                        axios.defaults.headers.common["Authorization"] = `Bearer ${response.data["access"]}`;
+                                        localStorage.setItem("access_token", response.data["access"]);
+                                        state.tokenData["access_token"] = response.data["access"];
+                                        state.isAuthenticated = true;
+                                    } else {
+                                        localStorage.removeItem("access_token");
+                                        localStorage.removeItem("refresh_token");
+                                        state.tokenData["access_token"] = "";
+                                        state.tokenData["refresh_token"] = "";
+                                        state.isAuthenticated = false;
+                                        axios.defaults.headers.common["Authorization"] = "";
+                                    }
+                                })
+                        }
+                    })
+            } else {
+                localStorage.removeItem("access_token");
+                localStorage.removeItem("refresh_token");
+                state.tokenData["access_token"] = "";
+                state.tokenData["refresh_token"] = "";
+                state.isAuthenticated = false;
+            }
+        },
+    },
     actions: {},
     modules: {}
 })
--- candy_store_client/src/views/LoginPage.vue
+++ candy_store_client/src/views/LoginPage.vue
@@ -9,6 +9,7 @@
       <b-form-input
           placeholder="Введите e-mail"
           class="custom-input login-page_field__input"
+          v-model="email"
       ></b-form-input>
     </div>
 
@@ -18,6 +19,7 @@
           placeholder="Введите пароль"
           class="custom-input login-page_field__input"
           type="password"
+          v-model="password"
       ></b-form-input>
     </div>
 
@@ -36,6 +38,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "LoginPage",
   mounted() {
@@ -43,8 +47,35 @@ export default {
   },
   data() {
     return {
-
+      email: "",
+      password: ""
     }
   },
+  methods: {
+    async login() {
+      const userData = {
+        "email": this.email,
+        "password": this.password,
+      };
+      await axios
+          .post("http://127.0.0.1:8000/api/v1/auth/login/", userData)
+          .then(response => {
+            if (response.data["access"] !== "" && response.data["refresh"] !== "") {
+              this.$store.commit("setToken", response.data);
+
+              axios.defaults.headers.common["Authorization"] = `Bearer ${response.data['access']}`;
+
+              localStorage.setItem("access_token", response.data["access"]);
+              localStorage.setItem("refresh_token", response.data["refresh"]);
+
+              const toPath = "/account";
+              this.$router.push(toPath);
+            } else {
+              console.log("Что-то пошло не так")
+            }
+          })
+          .catch(err => console.log(err))
+    },
+  }
 }
 </script>
